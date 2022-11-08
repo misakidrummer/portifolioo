5cce703e727d70e5e197156fb41d713fcfe37b94
Nathali Motooka <nanimotooka@gmail.com>
2022-11-08 17:09:39 -0300

    resolver problema do menu

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
     });
-
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-    });
-
+        // // toggle menu/navbar script
+        // $('.menu-btn').click(function(){
+        //     $('.navbar .menu').toggle("active");
+        // });
 });
+
